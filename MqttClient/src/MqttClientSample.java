1bbc650519ab39d6b10686b551512c72e400bff4
ahmedkhattab <khattab80@gmail.com>
2015-12-03 17:37:29 +0100

    testing link to hdfs in sparkapp

--- MqttClient/src/MqttClientSample.java
+++ MqttClient/src/MqttClientSample.java
@@ -1,49 +1,59 @@
+import java.util.Random;
+
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.JSONObject;
 
 public class MqttClientSample {
+	public static JSONObject getNewReading(String device) {
+		Random r = new Random();
 
-    public static void main(String[] args) {
+		JSONObject reading = new JSONObject();
+		reading.put("device", device);
+		reading.put("value", r.nextInt(10) + r.nextFloat());
+		reading.put("timestamp", System.currentTimeMillis());
+		System.out.println(reading.toString());
+		return reading;
+	}
 
-        String topic        = "sensor1";
-        String content      = "Message from MqttPublishSample";
-        int qos             = 1;
-        String broker       = String.format("tcp://52.29.49.89:31315");
-        String clientId     = "JavaSample";
-        MemoryPersistence persistence = new MemoryPersistence();
+	public static void main(String[] args) {
+		String[] devices = {"s1", "s2"};
+		int qos = 1;
+		String broker = String.format("tcp://52.29.211.135:31315");
+		String clientId = "JavaSample";
+		MemoryPersistence persistence = new MemoryPersistence();
 
-        try {
-            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
-            System.out.println("Connecting to broker: "+broker);
-            sampleClient.connect(connOpts);
-            System.out.println("Connected");
-            for(int i=0; i < 50; i++)
-            {
-	            System.out.println("Publishing message: "+content);
-	            MqttMessage message = new MqttMessage(content.getBytes());
-	            message.setQos(qos);
-	            sampleClient.publish(topic, message);
-	            System.out.println("Message published");
-	            Thread.sleep(1000);
-            }
-            sampleClient.disconnect();
-            System.out.println("Disconnected");
-            System.exit(0);
-        } catch(MqttException me) {
-            System.out.println("reason "+me.getReasonCode());
-            System.out.println("msg "+me.getMessage());
-            System.out.println("loc "+me.getLocalizedMessage());
-            System.out.println("cause "+me.getCause());
-            System.out.println("excep "+me);
-            me.printStackTrace();
-        } catch (InterruptedException e) {
-            System.out.println("reason "+e.getMessage());
+		try {
+			MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+			MqttConnectOptions connOpts = new MqttConnectOptions();
+			connOpts.setCleanSession(true);
+			System.out.println("Connecting to broker: " + broker);
+			sampleClient.connect(connOpts);
+			System.out.println("Connected");
+			for (int i = 0; i < 50; i++) {
+				System.out.println("Publishing message: " + getNewReading(devices[i%2]));
+				MqttMessage message = new MqttMessage(getNewReading(devices[i%2]).toString().getBytes());
+				message.setQos(qos);
+				sampleClient.publish(devices[i%2], message);
+				System.out.println("Message published");
+				Thread.sleep(1000);
+			}
+			sampleClient.disconnect();
+			System.out.println("Disconnected");
+			System.exit(0);
+		} catch (MqttException me) {
+			System.out.println("reason " + me.getReasonCode());
+			System.out.println("msg " + me.getMessage());
+			System.out.println("loc " + me.getLocalizedMessage());
+			System.out.println("cause " + me.getCause());
+			System.out.println("excep " + me);
+			me.printStackTrace();
+		} catch (InterruptedException e) {
+			System.out.println("reason " + e.getMessage());
 			e.printStackTrace();
 		}
-    }
+	}
 }
--- SparkApp/src/com/test/sparkapp/SparkApp.java
+++ SparkApp/src/com/test/sparkapp/SparkApp.java
@@ -1,5 +1,11 @@
 package com.test.sparkapp;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.mqtt.MQTTUtils;
 import org.apache.spark.streaming.Duration;
@@ -7,16 +13,29 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 public class SparkApp {
-public static void main(String[] args)
-{
-
-	String rabbitmq_address = String.format("tcp://%s:%s", System.getenv("RABBITMQ_SERVICE_HOST"), System.getenv("RABBITMQ_SERVICE_PORT_MQTT"));
-	System.out.println("connecting to" + rabbitmq_address);
-	SparkConf conf = new SparkConf().setAppName("testApp").setIfMissing("spark.master", "local[*]");
-	JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(1000));
-	JavaReceiverInputDStream<String> receiverStream = MQTTUtils.createStream(ssc, rabbitmq_address, "sensor1", org.apache.spark.storage.StorageLevel.MEMORY_ONLY());
-	receiverStream.print();
-    ssc.start();
-    ssc.awaitTermination();
-}
+	public static void main(String[] args) {
+		
+		String namenode = String.format("tcp://%s:50070", System.getenv("NAMENODE_SERVICE_HOST"));
+		
+		Configuration configuration = new Configuration();
+		try {
+			FileSystem hdfs = FileSystem.get( new URI(
+					namenode), configuration );
+		} catch (IOException | URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		String rabbitmq_address = String.format("tcp://%s:%s", System.getenv("RABBITMQ_SERVICE_HOST"),
+				System.getenv("RABBITMQ_SERVICE_PORT_MQTT"));
+		System.out.println("connecting to" + rabbitmq_address);
+		SparkConf conf = new SparkConf().setAppName("testApp").setIfMissing("spark.master", "local[*]");
+		JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(1000));
+		JavaReceiverInputDStream<String> receiverStream = MQTTUtils.createStream(ssc, rabbitmq_address, "s1",
+				org.apache.spark.storage.StorageLevel.MEMORY_ONLY());
+		receiverStream.print();
+		ssc.start();
+		ssc.awaitTermination();
+		ssc.stop();
+	}
 }

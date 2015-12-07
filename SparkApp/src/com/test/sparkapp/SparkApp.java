7a434e0f01fa1c1d2f555aa50f4d5fe5412f75e1
ahmedkhattab <khattab80@gmail.com>
2015-12-07 15:04:12 +0100

    hdfs trials

--- SparkApp/src/com/test/sparkapp/SparkApp.java
+++ SparkApp/src/com/test/sparkapp/SparkApp.java
@@ -3,10 +3,12 @@ package com.test.sparkapp;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.PrivilegedExceptionAction;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.SparkConf;
@@ -18,35 +20,44 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 public class SparkApp {
 	public static void main(String[] args) {
-		
-		String namenode = String.format("hdfs://%s:8020", System.getenv("NAMENODE_SERVICE_HOST"));
-		
-		Configuration configuration = new Configuration();
+
+		UserGroupInformation ugi = UserGroupInformation.createRemoteUser("hdfs");
 		try {
-			FileSystem hdfs = FileSystem.get( new URI(
-					namenode), configuration );
-		} catch (IOException | URISyntaxException e) {
+			ugi.doAs(new PrivilegedExceptionAction<Void>() {
+
+				public Void run() throws Exception {
+					String namenode = String.format("hdfs://%s:8020", System.getenv("NAMENODE_SERVICE_HOST"));
+
+					Configuration configuration = new Configuration();
+
+					FileSystem hdfs = FileSystem.get(new URI(namenode), configuration);
+
+					String rabbitmq_address = String.format("tcp://%s:%s", System.getenv("RABBITMQ_SERVICE_HOST"),
+							System.getenv("RABBITMQ_SERVICE_PORT_MQTT"));
+					System.out.println("connecting to" + rabbitmq_address);
+					SparkConf conf = new SparkConf().setAppName("testApp").setIfMissing("spark.master", "local[*]");
+					JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(1000));
+					JavaReceiverInputDStream<String> receiverStream = MQTTUtils.createStream(ssc, rabbitmq_address,
+							"s1", org.apache.spark.storage.StorageLevel.MEMORY_ONLY());
+					receiverStream.foreachRDD(new Function2<JavaRDD<String>, Time, Void>() {
+						@Override
+						public Void call(JavaRDD<String> rdd, Time time) {
+							if (!rdd.isEmpty())
+								rdd.saveAsTextFile(
+										String.format("hdfs://%s:8020", System.getenv("NAMENODE_SERVICE_HOST"))
+												+ "/user/hdfs/output/");
+							return null;
+
+						}
+					});
+					ssc.start();
+					ssc.awaitTermination();
+					ssc.stop();
+					return null;
+				}
+			});
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-				
-		String rabbitmq_address = String.format("tcp://%s:%s", System.getenv("RABBITMQ_SERVICE_HOST"),
-				System.getenv("RABBITMQ_SERVICE_PORT_MQTT"));
-		System.out.println("connecting to" + rabbitmq_address);
-		SparkConf conf = new SparkConf().setAppName("testApp").setIfMissing("spark.master", "local[*]");
-		JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(1000));
-		JavaReceiverInputDStream<String> receiverStream = MQTTUtils.createStream(ssc, rabbitmq_address, "s1",
-				org.apache.spark.storage.StorageLevel.MEMORY_ONLY());
-		receiverStream.foreachRDD( new Function2<JavaRDD<String>, Time, Void>() {
-		      @Override
-		      public Void call(JavaRDD<String> rdd, Time time) {
-		    	  if(!rdd.isEmpty())
-		    		  rdd.saveAsTextFile(String.format("hdfs://%s:8020", System.getenv("NAMENODE_SERVICE_HOST")) + "/user/hdfs/output.txt");
-		    	  return null;
-		    
-		      }
-		    });
-		ssc.start();
-		ssc.awaitTermination();
-		ssc.stop();
 	}
 }

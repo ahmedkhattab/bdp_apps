987939c07502d1407bae993f77dd94d06e372602
ahmedkhattab <khattab80@gmail.com>
2016-04-11 13:27:38 +0200

    updated spark app

--- SparkApp/src/com/test/sparkapp/SparkWordCount.java
+++ SparkApp/src/com/test/sparkapp/SparkWordCount.java
@@ -37,7 +37,7 @@ public final class SparkWordCount {
 
     SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
     JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-    JavaRDD<String> lines = ctx.textFile("text-input.txt", 1);
+    JavaRDD<String> lines = ctx.textFile(args[0], 1);
 
     JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
         @Override

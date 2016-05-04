87697fba1c912492c11bae582475537b9bef062e
ahmedkhattab <khattab80@gmail.com>
2016-05-04 16:39:21 +0200

    decompress and push to hdfs

--- HdfsClient/src/com/hdfs/client/HdfsClient.java
+++ HdfsClient/src/com/hdfs/client/HdfsClient.java
@@ -1,14 +1,16 @@
 package com.hdfs.client;
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.security.PrivilegedExceptionAction;
 import java.util.zip.ZipInputStream;
-
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileStatus;
 
@@ -34,15 +36,23 @@ public class HdfsClient {
 									 
 					 
 					    final ZipInputStream in = new ZipInputStream(new FileInputStream(args [0]));
-					    OutputStream out = fs.create(new Path("/user/hdfs/pagerank"));
-
-
+					    OutputStream out = new FileOutputStream(args [0].substring(0, args[0].length()-3));
+					    
+					  
+					    IOUtils.copy(in, out);
+				        IOUtils.closeQuietly(in);
+				        out.close();
+				        OutputStream outHDFS = fs.create(new Path("/user/hdfs/pagerank"));
+					    
+					    
+					    InputStream inHDFS = new BufferedInputStream(new FileInputStream(args [0].substring(0, args[0].length()-3)));
+				        
 					// Get configuration of Hadoop system
 					Configuration conf = new Configuration();
 					System.out.println("Connecting to -- " + conf.get("fs.defaultFS"));
 
 					// Copy file from local to HDFS
-					IOUtils.copyBytes(in, out, 4096, true);
+					org.apache.hadoop.io.IOUtils.copyBytes(inHDFS, outHDFS, 4096, true);
 					FileStatus[] status = fs.listStatus(new Path("/user/hdfs"));
 					for (int i = 0; i < status.length; i++) {
 						System.out.println(status[i].getPath());

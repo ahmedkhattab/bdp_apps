0ab0ce19ea3a7e4e218867ef41d82d29fcea903b
ahmedkhattab <khattab80@gmail.com>
2016-05-04 16:17:17 +0200

    pagerank data

--- HdfsClient/src/com/hdfs/client/HdfsClient.java
+++ HdfsClient/src/com/hdfs/client/HdfsClient.java
@@ -1,15 +1,8 @@
 package com.hdfs.client;
-import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.security.PrivilegedExceptionAction;
-import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipInputStream;
 
 import org.apache.hadoop.conf.*;
